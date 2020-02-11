8a04c98aca63f91d2dec769e03bf1ee6fd06fde0
carlosperate <carlosperate@users.noreply.github.com>
2020-02-11 23:56:33 +0000

    Protocol: Extract protocol into its own module.

---
+++ serial_pc_control/protocol.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Carlos Pereira Atencio
+# SPDX-License-Identifier: MIT
+"""
+Serial protocol.
+"""
+import re
+from typing import Tuple
+
+CMD_START = b"|@s-"
+CMD_START_RE = b"\|@s-%b@\|"
+CMD_TYPE_RE = b"\|@s-(.*?)@\|"
+CMD_END = b"|@e@|\n"
+CMD_END_RE = b"\|@e@\|"
+
+
+def init() -> None:
+    """Initialise the protocol. Nothing to do here yet."""
+    pass
+
+
+def parse_cmd(cmd_full_str: bytes) -> Tuple[str, str]:
+    if not cmd_full_str.startswith(CMD_START):
+        raise ValueError("Unexpected start of message.")
+
+    cmd_name_match = re.match(CMD_TYPE_RE, cmd_full_str)
+    if not cmd_name_match:
+        raise ValueError("Could not parse command type.")
+
+    cmd_name = cmd_name_match.groups()[0]
+    cmd_content_match = re.match((CMD_START_RE % cmd_name) + b"(.*?)" + CMD_END_RE, cmd_full_str, flags=re.S)
+    if not cmd_content_match:
+        raise ValueError("Could not parse command value.")
+
+    cmd_content = cmd_content_match.groups()[0]
+    cmd_content = cmd_content.decode("utf-8")
+    cmd_name = cmd_name.decode("utf-8")
+    return cmd_name, cmd_content
